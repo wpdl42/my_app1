5b2b8ac53246969558e031bda5bb04bb5ed468e7
wpdl42 <130218323+wpdl42@users.noreply.github.com>
2026-09-14 17:55:00 +0900

    1st

--- components/counter.tsx
+++ components/counter.tsx
@@ -9,7 +9,7 @@ export function Counter() {
       className="rounded-full bg-black/[.06] px-3 py-1 text-xs font-medium tracking-wide text-zinc-600 dark:bg-white/[.08] dark:text-zinc-300"
       onClick={() => setCount((c) => c + 1)}
     >
-      카운터 - {count}
+      카운터 -- {count}
     </button>
   );
 }
